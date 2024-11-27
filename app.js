87b278f8f7a762b60f670c7cd9e6b49b2a0a6103
Jennifer Boateng <jenniferboateng@Jennifers-Air.lan>
2024-11-27 21:55:39 +0000

    finish majority of mobile view dev

---
+++ app.js
@@ -0,0 +1,22 @@
+var navLinks = document.getElementsByClassName("nav-link");
+
+var closeBtn = document.getElementsByClassName("close-btn");
+
+// Toggle menu on and off
+
+toggleMenu = () => {
+  document.closeBtn.onClick;
+  if (navLinks.style.display === "block") {
+    navLinks.style.display = "none";
+  } else {
+  }
+};
+
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
